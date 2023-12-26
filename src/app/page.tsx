9a6aff08dd90638ab6aa0996e73001173347e882
Abdiza Alemu <abdisadev>
2023-12-26 23:13:26 +0300

    added react icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center  items-center h-screen items-center dark:bg-grey-300">
       <h1 className="text-9xl">Abdiza Alemu</h1>
       <p className="text-7xl">Developer.</p>
+      <Button>
+        <FaGithub className="text-xl" />
+      </Button>
     </div>
   );
 }
