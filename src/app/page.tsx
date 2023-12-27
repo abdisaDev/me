fcf1b9635a49f511a9c27c2d255c14658f2b3d22
Abdisa Alemu <abdisadev@gmail.com>
2023-12-27 08:51:54 +0300

    some cool stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaGithubAlt } from "react-icons/fa";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center  items-center h-screen items-center dark:bg-grey-300">
-      <h1 className="text-9xl">Abdiza Alemu</h1>
-      <p className="text-7xl">Developer.</p>
-      <Button>
-        <FaGithub className="text-xl" />
-      </Button>
+      <h1 className="text-9xl text-shadow-pop-tr">Abdisa Alemu</h1>
+      <p className="text-7xl focus-in-contract">
+        Devel
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Link
+                href="https://github.com/developed-by-abdiza"
+                target="blank"
+              >
+                <FaGithubAlt className="text-6xl inline jello-horizontal vibrate-1" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">My Github</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        per.
+      </p>
     </div>
   );
 }
