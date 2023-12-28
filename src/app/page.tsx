a9511b940341f770eaf41af9b08ad9a5e3e0ec8a
Abdisa Alemu <abdisadev@gmail.com>
2023-12-28 07:45:10 +0300

    responsive hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,11 @@ import {
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center  items-center h-screen items-center dark:bg-grey-300">
-      <h1 className="text-9xl text-shadow-pop-tr">Abdisa Alemu</h1>
-      <p className="text-7xl focus-in-contract">
+    <div className="flex flex-col justify-center items-center h-screen items-center dark:bg-grey-300 ">
+      <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-shadow-pop-tr text-center">
+        Abdisa Alemu
+      </h1>
+      <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl focus-in-contract ">
         Devel
         <TooltipProvider>
           <Tooltip>
@@ -23,10 +25,12 @@ export default function Home() {
                 href="https://github.com/developed-by-abdiza"
                 target="blank"
               >
-                <FaGithubAlt className="text-6xl inline jello-horizontal vibrate-1" />
+                <FaGithubAlt className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl inline jello-horizontal vibrate-1" />
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="bottom">My Github</TooltipContent>
+            <TooltipContent side="bottom" className="sm:text-sm text-xs">
+              Github
+            </TooltipContent>
           </Tooltip>
         </TooltipProvider>
         per.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,11 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "main-light": "#e5e7eb",
       },
     },
   },
   plugins: [],
+  darkMode: "class",
 };
 export default config;
