94ba81b112fee03f9a267a9afa9121b370275180
Abdiza Alemu <abdisadev>
2024-01-07 08:03:03 +0300

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,16 +1,13 @@
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
   return (
-    <div className="bg-stone-200 w-3/6 h-14 mx-auto rounded-br-lg rounded-bl-lg"></div>
+    <div className="bg-zinc-900 dark:bg-zinc-200 w-3/6 h-14 mx-auto rounded-br-lg rounded-bl-lg flex justify-center items-center ">
+      <Avatar>
+        <AvatarImage src="https://github.com/developed-by-abdiza.png" />
+        <AvatarFallback>ME</AvatarFallback>
+      </Avatar>
+    </div>
   );
 };
 
