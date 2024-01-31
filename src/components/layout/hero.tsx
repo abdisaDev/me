698e8f580f6b6cb671b6d72a856992abd3797ab4
Abdisa Alemu <abdisadev@gmail.com>
2024-01-31 08:45:00 +0300

    backdrop

--- src/components/layout/hero.tsx
+++ src/components/layout/hero.tsx
@@ -8,64 +8,68 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { Button } from "../ui/button";
+import BluryBox from "../styles/blury-box";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen items-center dark:bg-grey-300 ">
-      <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-shadow-pop-tr text-center">
-        Abdisa Alemu
-      </h1>
-      <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl focus-in-contract ">
-        Devel
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Link
-                href="https://github.com/developed-by-abdiza"
-                target="blank"
-              >
-                <FaGithubAlt className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl inline jello-horizontal vibrate-1 " />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="bottom" className="sm:text-sm text-xs">
-              Github
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        per.
-      </p>
-      <p className="mt-7 mb-1">Connect with me</p>
-      <hr className="w-64 h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+    <div className="relative">
+      <BluryBox />
 
-      <div>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Link
-                href="https://github.com/developed-by-abdiza"
-                target="blank"
-              >
-                <FaGithub className="text-xl sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl inline jello-horizontal vibrate-1 mr-3" />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="bottom" className="sm:text-sm text-xs">
-              Github
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Link href="https://twitter.com/abdisaDev" target="blank">
-                <FaXTwitter className="text-xl sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl inline jello-horizontal vibrate-1 " />
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="bottom" className="sm:text-sm text-xs">
-              X
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+      <div className="flex flex-col justify-center items-center h-screen items-center dark:bg-grey-300 backdrop-blur-lg">
+        <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-shadow-pop-tr text-center">
+          Abdisa Alemu
+        </h1>
+        <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl focus-in-contract ">
+          Devel
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href="https://github.com/developed-by-abdiza"
+                  target="blank"
+                >
+                  <FaGithubAlt className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl inline jello-horizontal vibrate-1 " />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" className="sm:text-sm text-xs">
+                Github
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          per.
+        </p>
+        <p className="mt-7 mb-1">Connect with me</p>
+        <hr className="w-64 h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+
+        <div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  href="https://github.com/developed-by-abdiza"
+                  target="blank"
+                >
+                  <FaGithub className="text-xl sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl inline jello-horizontal vibrate-1 mr-3" />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" className="sm:text-sm text-xs">
+                Github
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link href="https://twitter.com/abdisaDev" target="blank">
+                  <FaXTwitter className="text-xl sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl inline jello-horizontal vibrate-1 " />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="bottom" className="sm:text-sm text-xs">
+                X
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/styles/blury-box.tsx
@@ -0,0 +1,6 @@
+const BluryBox = () => {
+  return (
+    <div className="blur-3xl w-96 h-96 bg-blue-900 -z-50 absolute top-40 left-40 rounded-full opacity-70"></div>
+  );
+};
+export default BluryBox;
