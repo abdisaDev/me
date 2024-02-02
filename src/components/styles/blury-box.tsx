f9bf23acffb2df5b2ec071300d86e1111a6a7286
Abdisa Alemu <abdisadev@gmail.com>
2024-02-02 09:24:12 +0300

    backdrop filter

--- src/components/styles/blury-box.tsx
+++ src/components/styles/blury-box.tsx
@@ -1,6 +1,6 @@
 const BluryBox = () => {
   return (
-    <div className="blur-3xl w-5/6 h-4/6 bg-violet-900 -z-50 absolute top-40 left-40 rounded-full opacity-70"></div>
+    <div className="blur-3xl w-4/6 h-3/6 bg-violet-900 -z-50 absolute top-2/4 right-2/4 translate-x-2/4 -translate-y-2/4 rounded-full opacity-70"></div>
   );
 };
 export default BluryBox;
